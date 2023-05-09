575f0e34e7407812f256dd5601801823e9c4eb39
DESKTOP-2AG9GJT\Alejandro Cruz <alejandroxdlx@gmail.com>
2023-05-09 09:57:17 +0100

    commit inicial

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'notes-app-8c485',
+    appId: '1:712401282029:web:0938789b66233a31fafde4',
+    storageBucket: 'notes-app-8c485.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyDoHiy-OPleoAemerDkx4xJWFwvnyIUg20',
+    authDomain: 'notes-app-8c485.firebaseapp.com',
+    messagingSenderId: '712401282029',
+    measurementId: 'G-3KNTT0H545',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'notes-app-8c485',
+    appId: '1:712401282029:web:0938789b66233a31fafde4',
+    storageBucket: 'notes-app-8c485.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyDoHiy-OPleoAemerDkx4xJWFwvnyIUg20',
+    authDomain: 'notes-app-8c485.firebaseapp.com',
+    messagingSenderId: '712401282029',
+    measurementId: 'G-3KNTT0H545',
+  },
   production: false
 };
 
