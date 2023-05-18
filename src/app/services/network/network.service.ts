efed455dcb5b9783a4c51d4e0ca057c68ce1b0a1
Alejandro-M-Cruz <alejandromatiascc@gmail.com>
2023-05-18 16:40:09 +0100

    al recuperar conexión, se regresa a la ruta en que se encontraba al perder la conexión

--- src/app/services/network/network.service.ts
+++ src/app/services/network/network.service.ts
@@ -1,8 +1,9 @@
 import {Injectable, NgZone} from '@angular/core';
 import {Network} from "@capacitor/network";
-import {Router} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {PluginListenerHandle} from "@capacitor/core";
 import {ToastsService} from "../alerts/toasts.service";
+import {Location} from "@angular/common";
 
 @Injectable({
   providedIn: 'root'
@@ -10,17 +11,26 @@ import {ToastsService} from "../alerts/toasts.service";
 export class NetworkService {
   private networkListener?: PluginListenerHandle
   private previousConnectionStatus?: boolean
+  private routeWhereConnectionWasLost?: string
 
-  constructor(private toastsService: ToastsService, private router: Router, private ngZone: NgZone) { }
+  constructor(
+    private toastsService: ToastsService,
+    private location: Location,
+    private router: Router,
+    private ngZone: NgZone
+  ) { }
 
   isConnected(): Promise<boolean> {
     return Network.getStatus().then(status => status.connected)
   }
 
-  listenToNetworkChanges() {
-    this.networkListener = Network.addListener('networkStatusChange', status => {
-      this.onConnectionChangeEventFired(status.connected)
+  async listenToNetworkChanges() {
+    this.networkListener = Network.addListener('networkStatusChange', async status => {
+      await this.onConnectionChangeEventFired(status.connected)
     })
+    this.previousConnectionStatus = await this.isConnected()
+    if (!this.previousConnectionStatus)
+      await this.onAppStartedWithNoConnection()
   }
 
   private async onConnectionChangeEventFired(isConnected: boolean) {
@@ -30,6 +40,7 @@ export class NetworkService {
       this.previousConnectionStatus = isConnected
       if (!isConnected)
         await this.onAppStartedWithNoConnection()
+      return
     }
     this.previousConnectionStatus = isConnected
     this.ngZone.run(() => {
@@ -42,12 +53,13 @@ export class NetworkService {
   }
 
   private async onConnectionLost() {
+    this.routeWhereConnectionWasLost = this.location.path()
     await this.router.navigate(['/no-connection'])
     await this.toastsService.showErrorToast('Connection lost')
   }
 
   private async onConnectionRestored() {
-    await this.router.navigate(['/notes'])
+    await this.router.navigate([this.routeWhereConnectionWasLost ?? '/notes'])
     await this.toastsService.showSuccessToast('Connection restored')
   }
 
